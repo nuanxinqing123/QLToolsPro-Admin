d584aa1e87f739c95ea5dbe4b2ddc1b931bb6697
vs2pk0 <904239787@qq.com>
2022-10-04 11:17:01 +0800

    管理员总览优化

--- src/pages/adminPage/home/index.vue
+++ src/pages/adminPage/home/index.vue
@@ -3,22 +3,61 @@
  * @Author: LiLei
  * @Date: 2022-09-29 15:24:11
  * @LastEditors: LiLei
- * @LastEditTime: 2022-09-29 15:28:44
+ * @LastEditTime: 2022-10-04 11:15:22
 -->
 <template>
-    <div>
-        管理总览
-    </div>
+    <a-row>
+        <a-col :span="12">
+            <a-statistic title="当前生效的面板数量"
+                         :value="homeObj.panel_count"
+                         style="margin-right: 50px;margin-bottom: 50px;" />
+        </a-col>
+        <a-col :span="12">
+            <a-statistic title="当前生效的变量数量"
+                         :value="homeObj.env_count" />
+        </a-col>
+    </a-row>
+    <a-row>
+        <a-col :span="12">
+            <a-statistic title="网站注册用户数量"
+                         :value="homeObj.user_count"
+                         style="margin-right: 50px;margin-bottom: 50px;" />
+        </a-col>
+        <a-col :span="12">
+            <a-statistic title="网站会员用户数量"
+                         :value="homeObj.vip_user_count" />
+        </a-col>
+    </a-row>
+    <a-row>
+        <a-col :span="12">
+            <a-statistic title="今日消费积分数量"
+                         :value="homeObj.to_day_integral"
+                         style="margin-right: 50px;margin-bottom: 50px;" />
+        </a-col>
+        <a-col :span="12">
+            <a-statistic title="今日上传变量数量"
+                         :value="homeObj.to_day_upload_count" />
+        </a-col>
+    </a-row>
 </template>
 
 <script setup>
-import { reactive } from 'vue';
+import { ref } from 'vue';
 import {
     homeData
 } from "utils/api.js";
-
+const homeObj = ref({
+    "panel_count": 0, //当前生效的面板数量
+    "env_count": 0, //当前生效的变量数量
+    "user_count": 1, //网站注册用户数量
+    "vip_user_count": 0, //网站会员用户数量
+    "to_day_integral": 0, //今日消费积分数量
+    "to_day_upload_count": 0 //今日上传变量数量
+})
 const getData = () => {
-    homeData()
+    homeData().then(data => {
+        homeObj.value = data;
+    })
 }
 getData();
 </script>
